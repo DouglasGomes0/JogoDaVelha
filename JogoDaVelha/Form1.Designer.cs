455cfd7411d1b78cae6afb8da8764623e3c21804
DouglasGomes0 <douglasgomesaqui@gmail.com>
2025-05-19 15:07:09 -0300

    Initial commit

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEspaco1 = new System.Windows.Forms.Button();
+            this.btnEspaco2 = new System.Windows.Forms.Button();
+            this.btnEspaco3 = new System.Windows.Forms.Button();
+            this.btnEspaco4 = new System.Windows.Forms.Button();
+            this.btnEspaco5 = new System.Windows.Forms.Button();
+            this.btnEspaco6 = new System.Windows.Forms.Button();
+            this.btnEspaco7 = new System.Windows.Forms.Button();
+            this.btnEspaco8 = new System.Windows.Forms.Button();
+            this.btnEspaco9 = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.pibLogo = new System.Windows.Forms.PictureBox();
+            this.pibTicTacToe = new System.Windows.Forms.PictureBox();
+            this.lblVez = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibTicTacToe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEspaco1
+            // 
+            this.btnEspaco1.BackColor = System.Drawing.Color.White;
+            this.btnEspaco1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco1.Location = new System.Drawing.Point(85, 119);
+            this.btnEspaco1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco1.Name = "btnEspaco1";
+            this.btnEspaco1.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco1.TabIndex = 0;
+            this.btnEspaco1.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco2
+            // 
+            this.btnEspaco2.BackColor = System.Drawing.Color.White;
+            this.btnEspaco2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco2.Location = new System.Drawing.Point(206, 121);
+            this.btnEspaco2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco2.Name = "btnEspaco2";
+            this.btnEspaco2.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco2.TabIndex = 1;
+            this.btnEspaco2.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco3
+            // 
+            this.btnEspaco3.BackColor = System.Drawing.Color.White;
+            this.btnEspaco3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco3.Location = new System.Drawing.Point(326, 121);
+            this.btnEspaco3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco3.Name = "btnEspaco3";
+            this.btnEspaco3.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco3.TabIndex = 2;
+            this.btnEspaco3.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco4
+            // 
+            this.btnEspaco4.BackColor = System.Drawing.Color.White;
+            this.btnEspaco4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco4.Location = new System.Drawing.Point(86, 238);
+            this.btnEspaco4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco4.Name = "btnEspaco4";
+            this.btnEspaco4.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco4.TabIndex = 3;
+            this.btnEspaco4.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco5
+            // 
+            this.btnEspaco5.BackColor = System.Drawing.Color.White;
+            this.btnEspaco5.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco5.Location = new System.Drawing.Point(205, 235);
+            this.btnEspaco5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco5.Name = "btnEspaco5";
+            this.btnEspaco5.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco5.TabIndex = 4;
+            this.btnEspaco5.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco6
+            // 
+            this.btnEspaco6.BackColor = System.Drawing.Color.White;
+            this.btnEspaco6.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco6.Location = new System.Drawing.Point(326, 237);
+            this.btnEspaco6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco6.Name = "btnEspaco6";
+            this.btnEspaco6.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco6.TabIndex = 5;
+            this.btnEspaco6.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco7
+            // 
+            this.btnEspaco7.BackColor = System.Drawing.Color.White;
+            this.btnEspaco7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco7.Location = new System.Drawing.Point(83, 352);
+            this.btnEspaco7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco7.Name = "btnEspaco7";
+            this.btnEspaco7.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco7.TabIndex = 6;
+            this.btnEspaco7.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco8
+            // 
+            this.btnEspaco8.BackColor = System.Drawing.Color.White;
+            this.btnEspaco8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco8.Location = new System.Drawing.Point(205, 354);
+            this.btnEspaco8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco8.Name = "btnEspaco8";
+            this.btnEspaco8.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco8.TabIndex = 7;
+            this.btnEspaco8.UseVisualStyleBackColor = false;
+            // 
+            // btnEspaco9
+            // 
+            this.btnEspaco9.BackColor = System.Drawing.Color.White;
+            this.btnEspaco9.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEspaco9.Location = new System.Drawing.Point(326, 359);
+            this.btnEspaco9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEspaco9.Name = "btnEspaco9";
+            this.btnEspaco9.Size = new System.Drawing.Size(88, 81);
+            this.btnEspaco9.TabIndex = 8;
+            this.btnEspaco9.UseVisualStyleBackColor = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.Red;
+            this.btnFechar.Location = new System.Drawing.Point(427, 13);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(43, 44);
+            this.btnFechar.TabIndex = 11;
+            this.btnFechar.Text = "X";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.BackColor = System.Drawing.Color.White;
+            this.btnReiniciar.Font = new System.Drawing.Font("Comic Sans MS", 13.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReiniciar.ForeColor = System.Drawing.Color.Red;
+            this.btnReiniciar.Location = new System.Drawing.Point(4, 14);
+            this.btnReiniciar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(92, 44);
+            this.btnReiniciar.TabIndex = 12;
+            this.btnReiniciar.Text = "reload";
+            this.btnReiniciar.UseVisualStyleBackColor = false;
+            // 
+            // pibLogo
+            // 
+            this.pibLogo.Image = global::JogoDaVelha.Properties.Resources.tttmarqlrg;
+            this.pibLogo.Location = new System.Drawing.Point(100, 14);
+            this.pibLogo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pibLogo.Name = "pibLogo";
+            this.pibLogo.Size = new System.Drawing.Size(294, 93);
+            this.pibLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibLogo.TabIndex = 9;
+            this.pibLogo.TabStop = false;
+            // 
+            // pibTicTacToe
+            // 
+            this.pibTicTacToe.Image = global::JogoDaVelha.Properties.Resources._tictactoe;
+            this.pibTicTacToe.Location = new System.Drawing.Point(23, 83);
+            this.pibTicTacToe.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pibTicTacToe.Name = "pibTicTacToe";
+            this.pibTicTacToe.Size = new System.Drawing.Size(451, 380);
+            this.pibTicTacToe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibTicTacToe.TabIndex = 10;
+            this.pibTicTacToe.TabStop = false;
+            // 
+            // lblVez
+            // 
+            this.lblVez.AutoSize = true;
+            this.lblVez.Font = new System.Drawing.Font("Comic Sans MS", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVez.Location = new System.Drawing.Point(58, 466);
+            this.lblVez.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVez.Name = "lblVez";
+            this.lblVez.Size = new System.Drawing.Size(155, 34);
+            this.lblVez.TabIndex = 13;
+            this.lblVez.Text = "é a vez do X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 519);
+            this.Controls.Add(this.lblVez);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.pibLogo);
+            this.Controls.Add(this.btnEspaco9);
+            this.Controls.Add(this.btnEspaco8);
+            this.Controls.Add(this.btnEspaco7);
+            this.Controls.Add(this.btnEspaco6);
+            this.Controls.Add(this.btnEspaco5);
+            this.Controls.Add(this.btnEspaco4);
+            this.Controls.Add(this.btnEspaco3);
+            this.Controls.Add(this.btnEspaco2);
+            this.Controls.Add(this.btnEspaco1);
+            this.Controls.Add(this.pibTicTacToe);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibTicTacToe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEspaco1;
+        private System.Windows.Forms.Button btnEspaco2;
+        private System.Windows.Forms.Button btnEspaco3;
+        private System.Windows.Forms.Button btnEspaco4;
+        private System.Windows.Forms.Button btnEspaco5;
+        private System.Windows.Forms.Button btnEspaco6;
+        private System.Windows.Forms.Button btnEspaco7;
+        private System.Windows.Forms.Button btnEspaco8;
+        private System.Windows.Forms.Button btnEspaco9;
+        private System.Windows.Forms.PictureBox pibLogo;
+        private System.Windows.Forms.PictureBox pibTicTacToe;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label lblVez;
+    }
+}
+
